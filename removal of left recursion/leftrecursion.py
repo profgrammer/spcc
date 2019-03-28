8b8bfd8646d1fbe1d9a4eb3030d920a113f4411a
profgrammer <javarep4199@gmail.com>
2019-03-28 23:26:43 +0530

    left recursion: added removing indirect

--- removal of left recursion/leftrecursion.py
+++ removal of left recursion/leftrecursion.py
@@ -17,8 +17,45 @@ def clean(line):
     # print(left, right)
     return {"left": left, "right": right}
 
+
+# if S -> Xa and X -> Sb then replace X in S ie S -> Sba
+def step1(lines):
+    ans = list()
+    for i in range(len(lines)):
+        toReplace = False
+        line = lines[i]
+        s = line["left"]
+        newLeft = s
+        newRight = list()
+        for r in line["right"]:
+            x = r[0]
+            if x.isupper() and x != s:
+                for j in range(i+1, len(lines)):
+                    X = lines[j]["left"]
+                    if X == x:
+
+                        found = False
+                        for right in lines[j]["right"]:
+                            if right[0] == s:
+                                # print(s, x, X)
+                                found = True
+                                break
+                        if found:
+                            toReplace = True
+                            for right in lines[i]["right"]:
+                                if not right.startswith(X):
+                                    newRight.append(right)
+                            for right in lines[j]["right"]:
+                                newRight.append(right + r[1:])
+        if toReplace:
+            ans.append({"left": newLeft, "right": newRight})
+            # print(ans[-1])
+        else:
+            ans.append(lines[i])
+    return ans
+
 def removeLeftRecursion(line):
-    cleaned = clean(line)
+    cleaned = line
     left, right = cleaned["left"], cleaned["right"]
     alphas = list()
     betas = list()
@@ -47,6 +84,17 @@ def printProductions(rules):
         ans = ans[:-1]
         print(ans)
 
+
+lines = list()
 for line in f.readlines():
+    cleaned = clean(line)
+    lines.append(cleaned)
+lines = step1(lines)
+# print(lines)
+print("Step 1: removing indirect left recursion")
+printProductions(lines)
+print("After removing left recursion:")
+for line in lines:
     ans = removeLeftRecursion(line)
+    # print(ans)
     printProductions(ans)
